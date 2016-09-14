8cd77815caf92476294b4c747fe3c26b7217d937
Scott Penberthy <jsp@Scotts-MBP.home>
2016-09-13 23:01:31 -0400

    Address feedback in the first evaluation, adding in decay to alpha and gamma, tighter metrics, and a discussion on what it means to be an ideal policy.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -15,6 +15,8 @@ class LearningAgent(Agent):
         self.destination = None
         
         # TODO: Initialize any additional variables here
+        self.decay = False
+        self.epsilon = 0.0
         self.alpha = 0.2
         self.gamma = 0.8
         self.qdict = {}
@@ -72,7 +74,7 @@ class LearningAgent(Agent):
         if v is None:
            v = self.init_q(ix)
            self.qdict[ix] = v
-           print len(self.qdict.keys()), " states created: "
+#           print len(self.qdict.keys()), " states created: "
            self.show_state(0)
         return v
 
@@ -83,13 +85,29 @@ class LearningAgent(Agent):
         #
         return self.q()[action]
 
-    def policy_move(self):
+    def choose_epsilon(self,t):
+       #
+       # Per reviewer, introduce the option of
+       # decaying epsilon over time.  This follows
+       # one supposition that we're getting smarter
+       # and may not need randomness.
+       #
+       if self.decay:
+          return self.epsilon/max(float(t),1.0)
+       else:
+          return self.epsilon
+
+    def policy_move(self, t):
        #
        # The "policy" for predicting an action is the argmax_a Q(s,a). 
        # Return that value for the current state.
        #
        q_ = self.q()
-       return max(iter(q_), key = lambda action: q_[action])
+       e = self.choose_epsilon(t)
+       if (random.random() > e):
+           return max(iter(q_), key = lambda action: q_[action])
+       else:
+           return random.choice(self.env.valid_actions)
 
     def max_q(self):
        #
@@ -103,11 +121,24 @@ class LearningAgent(Agent):
        a = max(actions, key = lambda action: q_[action])
        return q_[a]
 
-    def lerp(self, old, newval):
+    def choose_alpha(self,t):
+       #
+       # Per reviewer, introduce the option of
+       # decaying alpha over time.  This follows
+       # one supposition that we're getting smarter
+       # and may not need randomness.
+       #
+       if self.decay:
+          return self.alpha/max(float(t),1.0)
+       else:
+          return self.alpha
+
+    def lerp(self, old, newval, t=1.0):
         #
         # linearly interpolate on alpha, implementing <-- alpha --
         #
-        return self.alpha*newval + (1-self.alpha)*old
+        a = self.choose_alpha(t)
+        return (1-a)*old + a*newval
 
     def reset(self, destination=None):
         # TODO: Prepare for a new trip; reset any variables here, if required
@@ -129,58 +160,102 @@ class LearningAgent(Agent):
 
         # TODO: Select action according to your policy
         q_hat = self.q()
-        action = self.policy_move()
+        action = self.policy_move(t)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
         self.rewards.append(self.rewards[-1]+reward)
 
-        # TODO: Learn policy based on state, action, reward
-        q_hat[action] = self.lerp(q_hat[action], reward + self.gamma * self.max_q())
+        # TODO: Learn policy based on state, action, reward.  Blend old and new value at a
+        # rate of alpha, the Bellman trick adopted from animation and control theory.
+        #
+        q_hat[action] = self.lerp(q_hat[action], reward + self.gamma * self.max_q(), t)
 
 
 import matplotlib.pyplot as plt
 
-def run():
+def run(count=500):
     """Run the agent for a finite number of trials."""
     data = []
 
-    for alpha in [0.2, 0.4, 0.6, 0.8]:
-        for gamma in [0.2, 0.4, 0.6, 0.8]:
+    for epsilon in [0.0]:   # this was best overall, after trying a whole bunch [0.05, 0.1, 0.15, 0.2]:
+        for alpha in [0.2, 0.4, 0.6, 0.8]:
+            for gamma in [0.2, 0.4, 0.6, 0.8]:
 
-            # Set up environment and agent
-            random.seed(0)
-            e = Environment()  # create environment (also adds some dummy traffic)
-            a = e.create_agent(LearningAgent)  # create agent
-            a.alpha = alpha
-            a.gamma = gamma
-            e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+                # Set up environment and agent
+                random.seed(0)
+                e = Environment()  # create environment (also adds some dummy traffic)
+                a = e.create_agent(LearningAgent)  # create agent
+                a.alpha = alpha
+                a.gamma = gamma
+                e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
 
-            # Now simulate it
-            sim = Simulator(e, update_delay=0, display=False)
-            # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+                # Now simulate it
+                sim = Simulator(e, update_delay=0, display=False)
+                # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-            trials = sim.run(n_trials=500)  # run for a specified number of trials
-            # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
-            data.append([alpha, gamma, trials, a.rewards, len(a.qdict.keys())])
+                trials = sim.run(n_trials=count)  # run for a specified number of trials
+                # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+                data.append([alpha, gamma, trials, a.rewards, len(a.qdict.keys()), epsilon])
+    print "\n\n"
     return data
 
-def show_data(data):
+def show_data(data, use_win_rate = False):
+    # collect data for our final report
+    best_score = 0
+    best_parms = None
+    report = {x:[] for x in 'epsilon alpha gamma mvps wins states penalty penalty_std'.split()}
     for entry in data:
-        alpha = entry[0]
-        gamma = entry[1]
-
-        trials = entry[2]
+        (alpha, gamma, trials, rewards, num_states, epsilon) = entry
         win = [trials[i][0] for i in range(0,len(trials))]
         steps = [trials[i][1] for i in range(0,len(trials))]
         score = [trials[i][2] for i in range(0,len(trials))]
         penalty = [trials[i][3] for i in range(0,len(trials))]
-        wins = sum(win)
-
-        print "alpha ", alpha, "gamma", gamma, "wins", wins, "states", entry[4]
+        win_rate = sum(win)/float(len(win))
         df = pd.DataFrame({'steps': steps, 'score': score, 'penalty': penalty})
-        print df.describe()
-        print
+        score_per_step = np.mean(df['score'])/np.mean(df['steps'])
 
+        report['epsilon'].append(epsilon)
+        report['alpha'].append(alpha)
+        report['gamma'].append(gamma)
+        report['mvps'].append(score_per_step)
+        report['wins'].append(win_rate)
+        report['states'].append(num_states)
+        report['penalty'].append(np.mean(penalty))
+        report['penalty_std'].append(np.std(penalty))
+
+        if use_win_rate:
+           barometer = win_rate
+        else:
+           barometer = score_per_step
+
+        if barometer > best_score:
+            best_score = barometer
+            best_parms = {'score': score_per_step, 'alpha': alpha, 'gamma': gamma,
+                          'epsilon': epsilon, 'states': num_states, 'win_rate': win_rate, 'data': df}
+
+
+    print "Mean Value Per Step by Varying alpha, gamma"
+    df = pd.DataFrame(report)
+    print df.pivot(index='alpha', columns='gamma', values='mvps')
+                   
+    print "\n\nOverall Win Rate by Varying alpha, gamma"
+    print df.pivot(index='alpha', columns='gamma', values='wins')
+
+    print "\n\nMean Penalty by Varying alpha, gamma"
+    print df.pivot(index='alpha', columns='gamma', values='penalty')
+
+    print "\nState exploration:"
+    print "Mean ", np.mean(report['states'])
+    print "Std ", np.std(report['states'])
+
+    print "\n\nBest in class: "
+    for k in best_parms.keys():
+        if k != 'data':
+            print '   ', k, best_parms[k]
+    print "    mean penalty", np.mean(best_parms['data']['penalty'])
+    print "    stdev penalty", np.std(best_parms['data']['penalty'])
+    
 if __name__ == '__main__':
     show_data(run())
+    print
